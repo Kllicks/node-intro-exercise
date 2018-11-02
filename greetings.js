cde223d8e63a4521fe067891fc660c418003d1eb
Kyle Sekellick <sekellickkyle@yahoo.com>
2018-11-02 11:37:59 -0400

    adds modules and uses them

--- greetings.js
+++ greetings.js
@@ -4,9 +4,9 @@ function hello(whom) {
     console.log(`hello ${whom}.`);
 }
 
-// function salutations(whom){
-//     console.log(``);
-// }
+function salutations(whom){
+    return `I am an alien, ${whom}`;
+}
 
 function goodbye(whom) {
     console.log(`goodbye ${whom}.`);
@@ -14,6 +14,7 @@ function goodbye(whom) {
 
 // 1. Export the hello function
 module.exports = {
-    hello,  // equivalent to hello: hello
+    // hello,  // equivalent to hello: hello
+    hello: salutations,
     goodbye // equivalent to goodbye: goodbye
 };
--- index.js
+++ index.js
@@ -2,7 +2,18 @@
 // const hello = require(`./greetings`);
 const {hello, goodbye} = require(`./greetings`);
 
+const cowsay = require(`cowsay`);
+
 // hello.hello(`me`);
 // hello.goodbye(`other me`);
 hello(`me`);
 goodbye(`other me`);
+
+let saying = cowsay.say({
+    // text : "I'm a coooooow",
+    text: hello(`me`),
+	e : "oO",
+	T : "U "
+})
+
+console.log(saying);
